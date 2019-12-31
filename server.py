c6fc1f12c79204c466fa1db08688b64febb07800
Gabben <gabben@protonmail.com>
2019-12-31 17:22:11 +0500

    Replacing message.reply with message.answer

--- server.py
+++ server.py
@@ -27,14 +27,13 @@ dp.middleware.setup(AccessMiddleware(ACCESS_ID))
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply(
+    await message.answer(
         "Бот для учёта финансов\n\n"
         "Добавить расход: 250 такси\n"
         "Сегодняшняя статистика: /today\n"
         "За текущий месяц: /month\n"
         "Последние внесённые расходы: /expenses\n"
-        "Категории трат: /categories",
-        reply=False)
+        "Категории трат: /categories")
 
 
 @dp.message_handler(commands=['categories'])
@@ -42,26 +41,26 @@ async def categories_list(message: types.Message):
     categories = Categories().get_all_categories()
     answer_message = "Категории трат:\n\n* " +\
             ("\n* ".join([c["name"]+' ('+", ".join(c["aliases"])+')' for c in categories]))
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler(commands=['today'])
 async def today_statistics(message: types.Message):
     answer_message = expenses.get_today_statistics()
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler(commands=['month'])
 async def month_statistics(message: types.Message):
     answer_message = expenses.get_month_statistics()
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler(commands=['expenses'])
 async def list_expenses(message: types.Message):
     last_expenses = expenses.last()
     if not last_expenses:
-        await message.reply("Расходы ещё не заведены", reply=False)
+        await message.answer("Расходы ещё не заведены")
         return
 
     last_expenses_rows = [
@@ -69,7 +68,7 @@ async def list_expenses(message: types.Message):
         f"/del{row['id']} для удаления"
         for row in last_expenses]
     answer_message = "Последние сохранённые траты:\n\n* " + "\n\n* ".join(last_expenses_rows)
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler(lambda message: message.text.startswith('/del'))
@@ -77,7 +76,7 @@ async def del_expense(message: types.Message):
     row_id = int(message.text[4:])
     expenses.delete_expense(row_id)
     answer_message = "Удалил"
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 @dp.message_handler()
@@ -85,12 +84,12 @@ async def add_expense(message: types.Message):
     try:
         expense = expenses.add_expense(message.text)
     except exceptions.NotCorrectMessage as e:
-        await message.reply(str(e), reply=False)
+        await message.answer(str(e))
         return
     answer_message = (
         f"Добавлены траты {expense.amount} руб на {expense.category_name}.\n\n"
         f"{expenses.get_today_statistics()}")
-    await message.reply(answer_message, reply=False)
+    await message.answer(answer_message)
 
 
 if __name__ == '__main__':
