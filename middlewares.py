dd5f9ed707b774f88d2ea7a11802413bc5c2f321
Alexey Goloburdin <sterx@rl6.ru>
2019-12-31 19:28:52 +0300

    рефакторинг — для аутентификации используется middleware aiogram, вместо message.reply используем message.answer и тд

---
+++ middlewares.py
@@ -0,0 +1,14 @@
+from aiogram import types
+from aiogram.dispatcher.handler import CancelHandler
+from aiogram.dispatcher.middlewares import BaseMiddleware
+
+
+class AccessMiddleware(BaseMiddleware):
+    def __init__(self, access_id: int):
+        self.access_id = access_id
+        super().__init__()
+
+    async def on_process_message(self, message: types.Message, _):
+        if int(message.from_user.id) != int(self.access_id):
+            await message.answer("Access Denied")
+            raise CancelHandler()
