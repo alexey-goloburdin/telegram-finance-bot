c585d4d82511a1b820babc7a389af9694f91eea9
Alexey Goloburdin <sterx@rl6.ru>
2019-12-31 16:23:28 +0300

    Небольшой рефакторинг, комменты в коде

--- expenses.py
+++ expenses.py
@@ -1,3 +1,4 @@
+""" Работа с расходами — их добавление, удаление, статистики"""
 import datetime
 import re
 from typing import NamedTuple, List, Dict
@@ -10,30 +11,35 @@ import exceptions
 
 
 class Message(NamedTuple):
+    """Структура распаршенного сообщения о новом расходе"""
     amount: int
     category_text: str
 
 
 class Expense(NamedTuple):
+    """Структура добавленного в БД нового расхода"""
     amount: int
     category_name: str
 
 
 def add_expense(raw_message: str) -> Expense:
+    """Добавляет новое сообщение.
+    Принимает на вход текст сообщения, пришедшего в бот."""
     parsed_message = _parse_message(raw_message)
     category = Categories().get_category(
         parsed_message.category_text)
     inserted_row_id = db.insert("expense", {
         "amount": parsed_message.amount,
         "created": _get_now_formatted(),
-        "category_codename": category["codename"],
+        "category_codename": category.codename,
         "raw_text": raw_message
     })
     return Expense(amount=parsed_message.amount,
-                   category_name=category["name"])
+                   category_name=category.name)
 
 
 def get_today_statistics() -> str:
+    """Возвращает строкой статистику расходов за сегодня"""
     cursor = db.get_cursor()
     cursor.execute("select sum(amount)"
                    "from expense where created=current_date")
@@ -54,6 +60,7 @@ def get_today_statistics() -> str:
 
 
 def get_month_statistics() -> str:
+    """Возвращает строкой статистику расходов за текущий месяц"""
     now = _get_now_datetime()
     first_day_of_month = f'{now.year:04d}-{now.month:02d}-01'
     cursor = db.get_cursor()
@@ -76,6 +83,7 @@ def get_month_statistics() -> str:
 
 
 def last():
+    """Возвращает последние несколько расходов"""
     cursor = db.get_cursor()
     cursor.execute(
         "select e.id, e.amount, c.name "
@@ -94,10 +102,12 @@ def last():
 
 
 def delete_expense(row_id: int) -> None:
+    """Удаляет сообщение по его идентификатору"""
     db.delete("expense", row_id)
 
 
 def _parse_message(raw_message: str) -> Message:
+    """Парсит текст пришедшего сообщения о новом расходе."""
     regexp_result = re.match(r"([\d ]+) (.*)", raw_message)
     if not regexp_result or not regexp_result.group(0) \
             or not regexp_result.group(1) or not regexp_result.group(2):
@@ -109,14 +119,16 @@ def _parse_message(raw_message: str) -> Message:
     category_text = regexp_result.group(2).strip().lower()
     return Message(amount=amount, category_text=category_text)
 
-
 def _get_now_formatted() -> str:
+    """Возвращает сегодняшнюю дату строкой"""
     return _get_now_datetime().strftime("%Y-%m-%d")
 
 def _get_now_datetime():
+    """Возвращает сегодняшний datetime с учётом времненной зоны Мск."""
     tz = pytz.timezone("Europe/Moscow")
     now = datetime.datetime.now(tz)
     return now
 
 def _get_budget_limit() -> int:
+    """Возвращает дневной лимит трат для основных базовых трат"""
     return db.fetchall("budget", ["daily_limit"])[0]["daily_limit"]
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Сервер Telegram бота, запускаемый непосредственно"""
 import os
 import logging
 
@@ -23,6 +24,7 @@ dp = Dispatcher(bot)
 
 
 def auth(func):
+    """Проверка отправителя сообщения. Разрешён только один отправитель."""
 
     async def wrapper(message):
         if message['from']['id'] != 135637452:
@@ -35,6 +37,7 @@ def auth(func):
 @dp.message_handler(commands=['start', 'help'])
 @auth
 async def send_welcome(message: types.Message):
+    """Отправляет приветственное сообщение и помощь по боту"""
     await message.reply(
         "Бот для учёта финансов\n\n"
         "Добавить расход: 250 такси\n"
@@ -45,18 +48,30 @@ async def send_welcome(message: types.Message):
         reply=False)
 
 
+@dp.message_handler(lambda message: message.text.startswith('/del'))
+@auth
+async def del_expense(message: types.Message):
+    """Удаляет одну запись о расходе по её идентификатору"""
+    row_id = int(message.text[4:])
+    expenses.delete_expense(row_id)
+    answer_message = "Удалил"
+    await message.reply(answer_message, reply=False)
+
+
 @dp.message_handler(commands=['categories'])
 @auth
 async def categories_list(message: types.Message):
+    """Отправляет список категорий расходов"""
     categories = Categories().get_all_categories()
     answer_message = "Категории трат:\n\n* " +\
-            ("\n* ".join([c["name"]+' ('+", ".join(c["aliases"])+')' for c in categories]))
+            ("\n* ".join([c.name+' ('+", ".join(c.aliases)+')' for c in categories]))
     await message.reply(answer_message, reply=False)
 
 
 @dp.message_handler(commands=['today'])
 @auth
 async def today_statistics(message: types.Message):
+    """Отправляет сегодняшнюю статистику трат"""
     answer_message = expenses.get_today_statistics()
     await message.reply(answer_message, reply=False)
 
@@ -64,6 +79,7 @@ async def today_statistics(message: types.Message):
 @dp.message_handler(commands=['month'])
 @auth
 async def month_statistics(message: types.Message):
+    """Отправляет статистику трат текущего месяца"""
     answer_message = expenses.get_month_statistics()
     await message.reply(answer_message, reply=False)
 
@@ -71,6 +87,7 @@ async def month_statistics(message: types.Message):
 @dp.message_handler(commands=['expenses'])
 @auth
 async def list_expenses(message: types.Message):
+    """Отправляет последние несколько записей о расходах"""
     last_expenses = expenses.last()
     if not last_expenses:
         await message.reply("Расходы ещё не заведены", reply=False)
@@ -84,18 +101,10 @@ async def list_expenses(message: types.Message):
     await message.reply(answer_message, reply=False)
 
 
-@dp.message_handler(lambda message: message.text.startswith('/del'))
-@auth
-async def del_expense(message: types.Message):
-    row_id = int(message.text[4:])
-    expenses.delete_expense(row_id)
-    answer_message = "Удалил"
-    await message.reply(answer_message, reply=False)
-
-
 @dp.message_handler()
 @auth
 async def add_expense(message: types.Message):
+    """Добавляет новый расход"""
     try:
         expense = expenses.add_expense(message.text)
     except exceptions.NotCorrectMessage as e:
