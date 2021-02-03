1e0bf5f224a91a5938316fb21b6deefe9602f62d
GravityTwoG <crytekov@gmail.com>
2021-02-03 18:27:15 +0700

    use python_dotenv,  make proxy optional

---
+++ dotenv_settings.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+from pathlib import Path  # Python 3.6+ only
+
+
+load_dotenv()
+
+# OR, the same with increased verbosity
+load_dotenv(verbose=True)
+
+# OR, explicitly providing path to '.env'
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 """Сервер Telegram бота, запускаемый непосредственно"""
+import dotenv_settings
 import logging
 import os
 
@@ -13,15 +14,22 @@ from middlewares import AccessMiddleware
 
 logging.basicConfig(level=logging.INFO)
 
-API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
-PROXY_AUTH = aiohttp.BasicAuth(
-    login=os.getenv("TELEGRAM_PROXY_LOGIN"),
-    password=os.getenv("TELEGRAM_PROXY_PASSWORD")
-)
+PROXY_LOGIN = os.getenv("TELEGRAM_PROXY_LOGIN")
+PROXY_PASSWORD = os.getenv("TELEGRAM_PROXY_PASSWORD")
+
+API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 ACCESS_ID = os.getenv("TELEGRAM_ACCESS_ID")
 
-bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
+if PROXY_URL is None:
+    bot = Bot(token=API_TOKEN)
+else:
+    PROXY_AUTH = aiohttp.BasicAuth(
+        login=PROXY_LOGIN,
+        password=PROXY_PASSWORD
+    )
+    bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
+
 dp = Dispatcher(bot)
 dp.middleware.setup(AccessMiddleware(ACCESS_ID))
 
